8c207cec7a49eef4b83bef01cd9eaf2b01e292de
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-08 17:17:11 +0600

    Add reactstrap and show modal dialog

--- src/app/components/AdminForm/index.tsx
+++ src/app/components/AdminForm/index.tsx
@@ -1,6 +1,30 @@
 import * as React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-export class AdminForm extends React.Component {
+export namespace AdminForm {
+  export interface Props {
+
+  }
+
+  export interface State {
+    modal: boolean;
+  }
+}
+
+export class AdminForm extends React.Component<AdminForm.Props, AdminForm.State> {
+  constructor(props: AdminForm.Props) {
+    super(props);
+    this.state = {
+      modal: false
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    })
+  };
+
   render() {
     return (
       <div className="container text-center">
@@ -34,7 +58,20 @@ export class AdminForm extends React.Component {
             </tr>
           </tbody>
         </table>
-        <div><button className="btn btn-primary">Новый товар</button></div>
+        <div><button className="btn btn-primary" onClick={this.toggle}>Новый товар</button></div>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
