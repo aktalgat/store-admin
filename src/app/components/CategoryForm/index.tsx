041294113683b68ff15c66c13e979a1f83c1812e
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-13 14:47:27 +0600

    New category dialog

--- src/app/components/CategoryForm/index.tsx
+++ src/app/components/CategoryForm/index.tsx
@@ -50,46 +50,8 @@ export class CategoryForm extends React.Component<CategoryForm.Props, CategoryFo
           <ModalBody>
             <form>
               <div className="form-group">
-                <label htmlFor="productName">Наименование</label>
-                <input type="text" className="form-control" id="productName" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="productDesc">Описание</label>
-                <textarea className="form-control" id="productDesc" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="productShortDesc">Краткое описание</label>
-                <textarea className="form-control" id="productShortDesc" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="productAddInfo">Дополнительная информация</label>
-                <textarea className="form-control" id="productAddInfo" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="productBadge">Значок</label>
-                <input type="text" className="form-control" id="productBadge" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="productPrice">Цена</label>
-                <input type="text" className="form-control" id="productPrice" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="productOldPrice">Старая цена</label>
-                <input type="text" className="form-control" id="productOldPrice" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="productStars">Звезды</label>
-                <select className="form-control" id="productStars">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div className="form-group">
-                <label htmlFor="productImages">Изображения</label>
-                <input type="file" className="form-control-file" id="productImages" />
+                <label htmlFor="categoryName">Наименование</label>
+                <input type="text" className="form-control" id="categoryName" />
               </div>
             </form>
           </ModalBody>
