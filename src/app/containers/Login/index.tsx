1218ae9e095fbfd01a7d03dc17ba67fb61632534
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-17 16:40:28 +0600

    Dispatch for Login container

--- src/app/containers/Login/index.tsx
+++ src/app/containers/Login/index.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
+import {connect} from "react-redux";
 import { RouteComponentProps } from 'react-router';
 import { LoginForm } from 'app/components';
+import {RootState} from "app/reducers";
+import {bindActionCreators, Dispatch} from "redux";
+import {LoginActions} from "app/actions";
 
 export namespace Login {
-  export interface Props extends RouteComponentProps<void> {}
+  export interface Props extends RouteComponentProps<void> {
+    messages: any;
+    error: string;
+    auth: any;
+  }
 }
 
+@connect(
+  (state: RootState): Pick<Login.Props, 'error' | 'messages'> => {
+    return {
+      error: '',
+      messages: state.intl.messages
+    };
+  },
+  (dispatch: Dispatch): Pick<Login.Props, 'auth'> => ({
+    auth: bindActionCreators(LoginActions.login, dispatch)
+  })
+)
 export class Login extends React.Component<Login.Props> {
   render() {
     return <LoginForm />;
