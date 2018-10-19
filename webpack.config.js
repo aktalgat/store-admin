bbfab836c7745d3a37e32e2956d032d2060066ff
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-19 16:40:52 +0600

    Add .env file for api url and devenv plugin

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,16 @@ const outPath = path.join(__dirname, './dist');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const dotenv = require('dotenv');
+
+// call dotenv and it will return an Object with a parsed key
+const env = dotenv.config().parsed;
+
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+return prev;
+}, {});
 
 module.exports = {
   context: sourcePath,
@@ -91,7 +101,8 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: 'assets/index.html'
-    })
+    }),
+    new webpack.DefinePlugin(envKeys)
   ],
   devServer: {
     contentBase: sourcePath,
