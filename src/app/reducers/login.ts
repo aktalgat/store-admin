53f09a110b2d6cbd03488a211efa98e9e67dd7a8
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-20 14:04:18 +0600

    Login reducer

---
+++ src/app/reducers/login.ts
@@ -0,0 +1,23 @@
+import {RootState} from "app/reducers/state";
+import {handleActions} from "redux-actions";
+import {LoginActions} from "app/actions";
+
+const initialState: RootState.UserState = {
+  userName: '',
+  email: '',
+  login: '',
+  error: ''
+};
+
+export const loginReducer = handleActions<RootState.UserState, any>(
+  {
+    [LoginActions.Type.LOGIN]: (state, action) => {
+      return { ...state, error: '' };
+    },
+    [LoginActions.Type.LOGIN_FAIL]: (state, action) => {
+      console.log('action: {}', action.payload);
+      return { ...state, error: action.payload };
+    }
+  },
+  initialState
+);
--- src/app/reducers/state.ts
+++ src/app/reducers/state.ts
@@ -1,11 +1,14 @@
 import { RouterState } from 'react-router-redux';
+import {UserModel} from "app/models";
 
 export interface RootState {
   router: RouterState;
   intl: any;
   locales: RootState.LocaleState;
+  user: RootState.UserState;
 }
 
 export namespace RootState {
   export type LocaleState = any;
+  export type UserState = UserModel
 }
