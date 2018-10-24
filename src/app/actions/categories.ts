eb6d3630e25d1e30a842f67a657e8587e2f199f3
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-24 13:03:28 +0600

    Categories actions

---
+++ src/app/actions/categories.ts
@@ -0,0 +1,15 @@
+import {createAction} from "redux-actions";
+
+export namespace CategoryActions {
+  export enum Type {
+    FETCH_CATEGORIES = 'FETCH_CATEGORIES',
+    REQUEST_CATEGORIES = 'REQUEST_CATEGORIES',
+    RECEIVE_CATEGORIES = 'RECEIVE_CATEGORIES',
+    RECEIVE_FAIL_CATEGORIES = 'RECEIVE_FAIL_CATEGORIES'
+  }
+
+  export const fetchCategories = createAction(Type.FETCH_CATEGORIES, (data: any) => data);
+  export const requestCategories = createAction(Type.REQUEST_CATEGORIES);
+  export const receiveCategories = createAction(Type.RECEIVE_CATEGORIES, (payments: any) => payments);
+  export const receiveCategoriesFail = createAction(Type.RECEIVE_FAIL_CATEGORIES, (error: any) => error);
+}
--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,2 +1,3 @@
 export * from './locales';
 export * from './login';
+export * from './categories'
