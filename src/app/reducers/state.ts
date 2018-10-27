5e291cb2a1a32537a9cb01b455db540d2a7f650f
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-27 10:56:24 +0600

    State add CategoriesModel

--- src/app/reducers/state.ts
+++ src/app/reducers/state.ts
@@ -1,14 +1,16 @@
 import { RouterState } from 'react-router-redux';
-import { UserModel } from 'app/models';
+import {CategoriesModel, UserModel} from 'app/models';
 
 export interface RootState {
   router: RouterState;
   intl: any;
   locales: RootState.LocaleState;
   user: RootState.UserState;
+  categories: RootState.CategoriesState;
 }
 
 export namespace RootState {
   export type LocaleState = any;
   export type UserState = UserModel;
+  export type CategoriesState = CategoriesModel;
 }
