b9cbe2e067bbd04f922ab386df01ee532130df9b
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-29 20:45:36 +0600

    Decode jwt token and set user state

--- src/app/reducers/login.ts
+++ src/app/reducers/login.ts
@@ -1,6 +1,7 @@
 import { RootState } from 'app/reducers/state';
 import { handleActions } from 'redux-actions';
 import { LoginActions } from 'app/actions';
+import * as jwt from 'jwt-decode';
 
 const initialState: RootState.UserState = {
   userName: '',
@@ -13,6 +14,16 @@ export const loginReducer = handleActions<RootState.UserState, any>(
   {
     [LoginActions.Type.LOGIN]: (state, action) => {
       return { ...state, error: '' };
+    },
+    [LoginActions.Type.LOGIN_DONE]: (state, action) => {
+      console.log('login done: {}', action);
+      let token: any = jwt(action.payload.response.accessToken);
+      let newState = {
+        userName: token.userName,
+        email: token.userEmail
+      };
+      console.log('token: {}', token);
+      return { ...state, newState}
     },
     [LoginActions.Type.LOGIN_FAIL]: (state, action) => {
       console.log('action: {}', action.payload);
