257c1866a4b70662def824b7279100533330e807
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-10-29 20:25:14 +0600

    Call fetch categories in Admin container

--- src/app/containers/Admin/index.tsx
+++ src/app/containers/Admin/index.tsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
 import { Redirect, RouteComponentProps } from 'react-router';
 import { AdminForm } from 'app/components';
+import { CategoryModel } from "app/models";
+import { connect } from "react-redux";
+import { RootState } from "app/reducers";
+import {bindActionCreators, Dispatch} from "redux";
+import {CategoriesActions} from "app/actions";
 
 export namespace Admin {
-  export interface Props extends RouteComponentProps<void> {}
+  export interface Props extends RouteComponentProps<void> {
+    categories: CategoryModel[],
+
+    fetch: any
+  }
 }
 
+@connect(
+  (state: RootState): Pick<Admin.Props, 'categories'> => {
+    return {
+      categories: state.categories.categories
+    }
+  },
+  (dispatch: Dispatch): Pick<Admin.Props, 'fetch'> => ({
+    fetch: bindActionCreators(CategoriesActions.fetchCategories, dispatch)
+  })
+)
 export class Admin extends React.Component<Admin.Props> {
+  componentDidMount() {
+    this.props.fetch();
+  }
+
   isAuth(): boolean {
     let token = sessionStorage.getItem('token') || '';
     return token != null && token !== '';
