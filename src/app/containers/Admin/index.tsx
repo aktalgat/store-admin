f9f298ca173d59bb67ac8243f606bd5912945f41
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-01 10:55:07 +0600

    Call check token

--- src/app/containers/Admin/index.tsx
+++ src/app/containers/Admin/index.tsx
@@ -1,31 +1,39 @@
 import * as React from 'react';
 import { Redirect, RouteComponentProps } from 'react-router';
 import { AdminForm } from 'app/components';
-import { CategoryModel } from "app/models";
+import {CategoryModel, UserModel} from "app/models";
 import { connect } from "react-redux";
 import { RootState } from "app/reducers";
 import {bindActionCreators, Dispatch} from "redux";
-import {CategoriesActions} from "app/actions";
+import {CategoriesActions, LoginActions} from "app/actions";
 
 export namespace Admin {
   export interface Props extends RouteComponentProps<void> {
     categories: CategoryModel[],
+    user: UserModel,
 
-    fetch: any
+    fetch: any,
+    checkToken: any
   }
 }
 
 @connect(
-  (state: RootState): Pick<Admin.Props, 'categories'> => {
+  (state: RootState): Pick<Admin.Props, 'categories' | 'user'> => {
     return {
-      categories: state.categories.categories
+      categories: state.categories.categories,
+      user: state.user
     }
   },
-  (dispatch: Dispatch): Pick<Admin.Props, 'fetch'> => ({
-    fetch: bindActionCreators(CategoriesActions.fetchCategories, dispatch)
+  (dispatch: Dispatch): Pick<Admin.Props, 'fetch' | 'checkToken'> => ({
+    fetch: bindActionCreators(CategoriesActions.fetchCategories, dispatch),
+    checkToken: bindActionCreators(LoginActions.checkToken, dispatch)
   })
 )
 export class Admin extends React.Component<Admin.Props> {
+  componentWillMount() {
+    this.props.checkToken();
+  }
+
   componentDidMount() {
     this.props.fetch();
   }
