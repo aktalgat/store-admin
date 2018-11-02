7a5d34743655051d2ba58198e745d1163b8cc3a0
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-02 16:23:27 +0600

    Fetch post to save category

--- src/app/api/categories.ts
+++ src/app/api/categories.ts
@@ -1,5 +1,9 @@
-import { fetchGET } from 'app/api/helper';
+import { fetchGET, fetchPOST } from 'app/api/helper';
 
 export const get = (data: any) => {
   return fetchGET('api/protected/categories', data);
+};
+
+export const post = (data: any) => {
+  return fetchPOST('api/protected/categories', data);
 };
