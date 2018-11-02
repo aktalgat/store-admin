0f5cec6504e6ac870cb1fc0be34792ae8efab782
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-02 16:30:01 +0600

    Add category actions

--- src/app/actions/categories.ts
+++ src/app/actions/categories.ts
@@ -5,11 +5,21 @@ export namespace CategoriesActions {
     FETCH_CATEGORIES = 'FETCH_CATEGORIES',
     REQUEST_CATEGORIES = 'REQUEST_CATEGORIES',
     RECEIVE_CATEGORIES = 'RECEIVE_CATEGORIES',
-    RECEIVE_FAIL_CATEGORIES = 'RECEIVE_FAIL_CATEGORIES'
+    RECEIVE_FAIL_CATEGORIES = 'RECEIVE_FAIL_CATEGORIES',
+
+    ADD_CATEGORY = "ADD_CATEGORY",
+    ADD_CATEGORY_REQUEST = "ADD_CATEGORY_REQUEST",
+    ADD_CATEGORY_DONE = "ADD_CATEGORY_DONE",
+    ADD_CATEGORY_FAIL = "ADD_CATEGORY_FAIL"
   }
 
   export const fetchCategories = createAction(Type.FETCH_CATEGORIES, (data: any) => data);
   export const requestCategories = createAction(Type.REQUEST_CATEGORIES);
   export const receiveCategories = createAction(Type.RECEIVE_CATEGORIES, (payments: any) => payments);
   export const receiveCategoriesFail = createAction(Type.RECEIVE_FAIL_CATEGORIES, (error: any) => error);
+
+  export const addCategory = createAction(Type.ADD_CATEGORY, (data: any) => data);
+  export const addCategoryRequest = createAction(Type.ADD_CATEGORY_REQUEST, (params: any) => params);
+  export const addCategoryDone = createAction(Type.ADD_CATEGORY_DONE, (data: any) => data);
+  export const addCategoryFail = createAction(Type.ADD_CATEGORY_FAIL, (error: any) => error);
 }
