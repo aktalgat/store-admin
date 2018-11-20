8f14324f4f1e7ad6d79d263eb312dc9369b2f609
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2018-11-20 19:20:04 +0600

    HeaderBar component for header

---
+++ src/app/components/HeaderBar/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+export namespace HeaderBar {
+  export interface Methods {
+    updateIntl: any;
+  }
+
+  export interface Fields {
+    currentLocale: any;
+    locales: any;
+  }
+
+  export interface Props extends Methods, Fields {}
+}
+
+export class HeaderBar extends React.Component<HeaderBar.Props> {
+  handleLangClick = (locale: any) => {
+    this.props.updateIntl({
+      locale: locale,
+      messages: this.props.locales[locale]
+    });
+  };
+
+  render() {
+    const { currentLocale, locales } = this.props;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          Todo
+        </Link>
+        <div className="nav navbar-nav ml-auto">
+          {Object.keys(locales).map((locale) => (
+            <button
+              key={locale}
+              className={'btn btn-sm ' + (currentLocale == locale ? 'btn-primary' : 'btn-outline-secondary')}
+              onClick={() => this.handleLangClick(locale)}>
+              {locales[locale].lang}
+            </button>
+          ))}
+        </div>
+      </nav>
+    );
+  }
+}
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,3 +3,4 @@ export { LoginForm } from './LoginForm';
 export { AdminForm } from './AdminForm';
 export { ProductForm } from './ProductForm';
 export { CategoryForm } from './CategoryForm';
+export { HeaderBar } from './HeaderBar';
