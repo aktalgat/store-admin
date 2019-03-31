437355469e31d98c8a9e48135646202fa4caf54d
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-03-31 16:16:31 +0600

    Header bar apply i18n

--- src/app/components/HeaderBar/index.tsx
+++ src/app/components/HeaderBar/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 
 export namespace HeaderBar {
@@ -31,7 +32,8 @@ export class HeaderBar extends React.Component<HeaderBar.Props> {
   render() {
     const { currentLocale, locales } = this.props;
     const logoutButton = !this.props.isExpired ?
-      <button type="button" className="btn btn-sm btn-outline-secondary" onClick={this.handleLogout}>Logout</button>
+      <button type="button" className="btn btn-sm btn-outline-secondary" onClick={this.handleLogout}>
+        <FormattedMessage id="logout" defaultMessage="Log out" /></button>
       : '';
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
