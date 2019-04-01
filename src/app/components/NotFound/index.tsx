59bbb7f9d4156f16d542bc2a3bd9cc245d04a0e4
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-04-01 16:21:09 +0600

    Not found page apply i18n

--- src/app/components/NotFound/index.tsx
+++ src/app/components/NotFound/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
 
 export class NotFound extends React.Component {
   render() {
@@ -6,7 +7,7 @@ export class NotFound extends React.Component {
       <div className="text-center d-flex">
         <div className="not-found-content">
           <h1>404</h1>
-          <h3>Page not found</h3>
+          <h3><FormattedMessage id="pageNotFound" defaultMessage="Page not found" /></h3>
         </div>
       </div>
     );
