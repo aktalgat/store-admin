cffd4590550cd5caf455dc7e1fc837516bbc8638
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-04-14 11:56:29 +0600

    Product use ProductModel instead of small properties

--- src/app/components/ProductForm/index.tsx
+++ src/app/components/ProductForm/index.tsx
@@ -15,17 +15,7 @@ export namespace ProductForm {
   export interface State {
     modal: boolean;
 
-    productId: number;
-    categoryId: number;
-    name: string;
-    description: string;
-    shortDescription: string;
-    additionalInfo: string;
-    badge: string;
-    price: number;
-    priceOld: number;
-    stars: number;
-    productImageList: string[];
+    product: ProductModel
     imageUrl: string;
   }
 }
@@ -33,26 +23,26 @@ export namespace ProductForm {
 export class ProductForm extends React.Component<ProductForm.Props, ProductForm.State> {
   constructor(props: ProductForm.Props) {
     super(props);
+    const product: ProductModel = this.createEmptyProduct();
     this.state = {
-      productId: 0,
       modal: false,
-      categoryId: 0,
-      name: '',
-      description: '',
-      shortDescription: '',
-      additionalInfo: '',
-      badge: '',
-      price: 0,
-      priceOld: 0,
-      stars: 1,
-      productImageList: [],
+      product: product,
+
       imageUrl: ''
     };
   }
 
+  createEmptyProduct = (): ProductModel => {
+    return { id: 0, categoryId: this.props.categories.length > 0 ? this.props.categories[0].id : 0,
+      name: '', description: '', shortDescription: '',
+      additionalInfo: '', badge: '', price: 0, priceOld: 0, stars: 1, productImageList: [] };
+  };
+
   componentWillReceiveProps(nextProps: ProductForm.Props) {
-    if (nextProps.categories.length > 0 && this.state.categoryId != nextProps.categories[0].id) {
-      this.setState({ categoryId: nextProps.categories[0].id });
+    let newProduct = {...this.state.product};
+    if (nextProps.categories.length > 0 && newProduct.categoryId != nextProps.categories[0].id) {
+      newProduct.categoryId = nextProps.categories[0].id;
+      this.setState({ product: newProduct });
     }
   }
 
@@ -66,38 +56,11 @@ export class ProductForm extends React.Component<ProductForm.Props, ProductForm.
   };
 
   handleEditClick = (product: ProductModel) => {
-    this.setState({
-      productId: product.id ? product.id : 0,
-      categoryId: product.categoryId,
-      name: product.name,
-      description: product.description,
-      shortDescription: product.shortDescription,
-      additionalInfo: product.additionalInfo,
-      badge: product.badge,
-      price: product.price,
-      priceOld: product.priceOld,
-      stars: product.stars,
-      productImageList: product.productImageList.map(item => item.url),
-      imageUrl: '',
-      modal: true
-    });
+    this.setState({ product: product, modal: true });
   };
 
   clearState = () => {
-    this.setState({
-      productId: 0,
-      categoryId: this.props.categories[0].id,
-      name: '',
-      description: '',
-      shortDescription: '',
-      additionalInfo: '',
-      badge: '',
-      price: 0,
-      priceOld: 0,
-      stars: 1,
-      productImageList: [],
-      imageUrl: ''
-    });
+    this.setState({ product: this.createEmptyProduct() })
   };
 
   getList = () => {
@@ -131,12 +94,6 @@ export class ProductForm extends React.Component<ProductForm.Props, ProductForm.
     });
   };
 
-  getImgListString = (imageUrls: string[]) => {
-    return imageUrls.map((item, index) => {
-      return <img key={index} src={item} width={100} height={100} />;
-    });
-  };
-
   getCategoryList = () => {
     return this.props.categories.map((item) => {
       return (
@@ -148,61 +105,66 @@ export class ProductForm extends React.Component<ProductForm.Props, ProductForm.
   };
 
   handleAddProduct = () => {
-    let product = {
-      id: this.state.productId,
-      name: this.state.name,
-      categoryId: this.state.categoryId,
-      description: this.state.description,
-      shortDescription: this.state.shortDescription,
-      additionalInfo: this.state.additionalInfo,
-      badge: this.state.badge,
-      price: this.state.price,
-      priceOld: this.state.priceOld,
-      stars: this.state.stars,
-      productImageList: this.state.productImageList
-    };
-    if (this.state.productId == 0) {
-      this.props.addProduct(product);
+    if (this.state.product.id == 0) {
+      this.props.addProduct(this.state.product);
     } else {
-      this.props.updateProduct(product);
+      this.props.updateProduct(this.state.product);
     }
     this.clearState();
   };
 
   handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ name: e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.name = e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({ categoryId: +e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.categoryId = +e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    this.setState({ description: e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.description = e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handleShortDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    this.setState({ shortDescription: e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.shortDescription = e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handleAddInfoChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    this.setState({ additionalInfo: e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.additionalInfo = e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handleBadgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ badge: e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.badge = e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ price: +e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.price = +e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handlePriceOldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ priceOld: +e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.priceOld = +e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handleStarsChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    this.setState({ stars: +e.target.value });
+    let newProduct = { ...this.state.product };
+    newProduct.stars = +e.target.value;
+    this.setState({ product: newProduct });
   };
 
   handleImageUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -210,9 +172,11 @@ export class ProductForm extends React.Component<ProductForm.Props, ProductForm.
   };
 
   handleImageAddMore = () => {
-    let list = this.state.productImageList;
-    list.push(this.state.imageUrl);
-    this.setState({ productImageList: list, imageUrl: '' });
+    let newProduct = { ...this.state.product };
+    let list = newProduct.productImageList;
+    list.push({ id: 0, url: this.state.imageUrl });
+    newProduct.productImageList = list;
+    this.setState({ product: newProduct, imageUrl: '' });
   };
 
   render() {
@@ -255,53 +219,53 @@ export class ProductForm extends React.Component<ProductForm.Props, ProductForm.
             <form>
               <div className="form-group">
                 <label htmlFor="productName"><FormattedMessage id="nomination" defaultMessage="Name" /></label>
-                <input type="text" className="form-control" id="productName" defaultValue={this.state.name}
+                <input type="text" className="form-control" id="productName" defaultValue={this.state.product.name}
                        onChange={this.handleNameChange}/>
               </div>
               <div className="form-group">
                 <label htmlFor="category"><FormattedMessage id="category" defaultMessage="Category" /></label>
-                <select className="form-control" id="category" defaultValue={this.state.categoryId.toString()}
+                <select className="form-control" id="category" defaultValue={this.state.product.categoryId.toString()}
                         onChange={this.handleCategoryChange}>
                   {this.getCategoryList()}
                 </select>
               </div>
               <div className="form-group">
                 <label htmlFor="productDesc"><FormattedMessage id="description" defaultMessage="Description" /></label>
-                <textarea className="form-control" id="productDesc" defaultValue={this.state.description}
+                <textarea className="form-control" id="productDesc" defaultValue={this.state.product.description}
                           onChange={this.handleDescriptionChange}/>
               </div>
               <div className="form-group">
                 <label htmlFor="productShortDesc">
                   <FormattedMessage id="shortDescription" defaultMessage="Short description" />
                 </label>
-                <textarea className="form-control" id="productShortDesc" defaultValue={this.state.shortDescription}
+                <textarea className="form-control" id="productShortDesc" defaultValue={this.state.product.shortDescription}
                           onChange={this.handleShortDescriptionChange}/>
               </div>
               <div className="form-group">
                 <label htmlFor="productAddInfo">
                   <FormattedMessage id="extraInfo" defaultMessage="Extra information" />
                 </label>
-                <textarea className="form-control" id="productAddInfo" defaultValue={this.state.additionalInfo}
+                <textarea className="form-control" id="productAddInfo" defaultValue={this.state.product.additionalInfo}
                           onChange={this.handleAddInfoChange}/>
               </div>
               <div className="form-group">
                 <label htmlFor="productBadge"><FormattedMessage id="badge" defaultMessage="Badge" /></label>
-                <input type="text" className="form-control" id="productBadge" defaultValue={this.state.badge}
+                <input type="text" className="form-control" id="productBadge" defaultValue={this.state.product.badge}
                        onChange={this.handleBadgeChange}/>
               </div>
               <div className="form-group">
                 <label htmlFor="productPrice"><FormattedMessage id="price" defaultMessage="Price" /></label>
-                <input type="text" className="form-control" id="productPrice" defaultValue={this.state.price.toString()}
+                <input type="text" className="form-control" id="productPrice" defaultValue={this.state.product.price.toString()}
                        onChange={this.handlePriceChange}/>
               </div>
               <div className="form-group">
                 <label htmlFor="productOldPrice"><FormattedMessage id="oldPrice" defaultMessage="Old price" /></label>
-                <input type="text" className="form-control" id="productOldPrice" defaultValue={this.state.priceOld.toString()}
+                <input type="text" className="form-control" id="productOldPrice" defaultValue={this.state.product.priceOld.toString()}
                        onChange={this.handlePriceOldChange}/>
               </div>
               <div className="form-group">
                 <label htmlFor="productStars"><FormattedMessage id="stars" defaultMessage="Stars" /></label>
-                <select className="form-control" id="productStars" defaultValue={this.state.stars.toString()}
+                <select className="form-control" id="productStars" defaultValue={this.state.product.stars.toString()}
                         onChange={this.handleStarsChange}>
                   <option>1</option>
                   <option>2</option>
@@ -321,14 +285,14 @@ export class ProductForm extends React.Component<ProductForm.Props, ProductForm.
                 </Button>
               </div>
               <div>
-                {this.getImgListString(this.state.productImageList)}
+                {this.getImgList(this.state.product.productImageList)}
               </div>
             </form>
           </ModalBody>
           <ModalFooter>
             <Button color="primary" onClick={() => { this.toggle(); this.handleAddProduct(); } }>
-              <FormattedMessage id={this.state.productId == 0 ? "add" : "edit"}
-                                defaultMessage={this.state.productId == 0 ? "Add" : "Edit"} />
+              <FormattedMessage id={this.state.product.id == 0 ? "add" : "edit"}
+                                defaultMessage={this.state.product.id == 0 ? "Add" : "Edit"} />
             </Button>{' '}
             <Button color="secondary" onClick={this.toggle}>
               <FormattedMessage id="cancel" defaultMessage="Cancel" />
