041d7177ef2e2259eceb1326c83fe7189cc5b098
Talgat Akunsartov <talgat.akunsartov@gmail.com>
2019-04-18 14:33:58 +0600

    Actions for updating category

--- src/app/actions/categories.ts
+++ src/app/actions/categories.ts
@@ -10,7 +10,12 @@ export namespace CategoriesActions {
     ADD_CATEGORY = 'ADD_CATEGORY',
     ADD_CATEGORY_REQUEST = 'ADD_CATEGORY_REQUEST',
     ADD_CATEGORY_DONE = 'ADD_CATEGORY_DONE',
-    ADD_CATEGORY_FAIL = 'ADD_CATEGORY_FAIL'
+    ADD_CATEGORY_FAIL = 'ADD_CATEGORY_FAIL',
+
+    UPDATE_CATEGORY = 'UPDATE_CATEGORY',
+    UPDATE_CATEGORY_REQUEST = 'UPDATE_CATEGORY_REQUEST',
+    UPDATE_CATEGORY_DONE = 'UPDATE_CATEGORY_DONE',
+    UPDATE_CATEGORY_FAIL = 'UPDATE_CATEGORY_FAIL'
   }
 
   export const fetchCategories = createAction(Type.FETCH_CATEGORIES, (data: any) => data);
@@ -33,5 +38,16 @@ export namespace CategoriesActions {
   export const addCategoryFail = createAction(
     Type.ADD_CATEGORY_FAIL,
     (error: any) => 'Add category. ' + error + '. '
+  );
+
+  export const updateCategory = createAction(Type.UPDATE_CATEGORY, (data: any) => data);
+  export const updateCategoryRequest = createAction(
+    Type.UPDATE_CATEGORY_REQUEST,
+    (params: any) => params
+  );
+  export const updateCategoryDone = createAction(Type.UPDATE_CATEGORY_DONE, (data: any) => data);
+  export const updateCategoryFail = createAction(
+    Type.UPDATE_CATEGORY_FAIL,
+    (error: any) => 'Update category. ' + error + '. '
   );
 }
